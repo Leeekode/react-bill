ccca93c4ad81aa68b8101d599ad00abd0cb6ee70
LLEON <140275669+Leeekode@users.noreply.github.com>
2024-04-24 22:33:52 -0400

    使用RTK管理账单列表

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router';
 import './theme.css'
+import store from './store'
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>
+    <Provider store={store}>
+         <RouterProvider router={router} />
+    </Provider>
+ 
 );
